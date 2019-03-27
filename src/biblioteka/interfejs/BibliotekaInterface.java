2d74d16beba21fa33aa97f10596bb87d725088ad
bojantomic <tomicb@fon.rs>
2019-03-27 11:32:37 +0100

    Uveden interfejs BibliotekaInterface

---
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -0,0 +1,19 @@
+package biblioteka.interfejs;
+
+import java.util.List;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterface {
+	
+	public void dodajKnjigu(Knjiga k);
+	
+	public void obrisiKnjigu(Knjiga k);
+	
+	public List<Knjiga> vratiSveKnjige();
+	
+	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, 
+			String naslov, String izdavac);
+	
+}
