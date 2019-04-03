d78f73d990ace4e00a6e9e2285bdb3fa303a737a
bojantomic <tomicb@fon.rs>
2019-04-03 10:33:58 +0200

    Implemenetiran interfejs u klasi Biblioteka

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,38 @@
+package biblioteka;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+	
+	private List<Knjiga> knjige = new LinkedList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga k) {
+		knjige.add(k);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga k) {
+		knjige.remove(k);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
+		
+		for (Knjiga knjiga: knjige)
+			if (knjiga.getNaslov().contains(naslov))
+				rezultati.add(knjiga);
+		
+		return rezultati;
+	}
+
+}
